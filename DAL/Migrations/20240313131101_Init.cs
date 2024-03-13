6c5e90e6e4ccb182047f70bd8aeccfb4c6b96803
Wiktor-Kobryn <92624464+Wiktor-Kobryn@users.noreply.github.com>
2024-03-13 14:26:04 +0100

    dodanie DAL

---
+++ DAL/Migrations/20240313131101_Init.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PRODUCTS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUCTS", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "USERS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USERS", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BASKET_POSITIONS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BASKET_POSITIONS", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BASKET_POSITIONS_PRODUCTS_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "PRODUCTS",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BASKET_POSITIONS_USERS_UserID",
+                        column: x => x.UserID,
+                        principalTable: "USERS",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ORDERS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORDERS", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ORDERS_USERS_UserID",
+                        column: x => x.UserID,
+                        principalTable: "USERS",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ORDER_POSITIONS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORDER_POSITIONS", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ORDER_POSITIONS_ORDERS_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "ORDERS",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BASKET_POSITIONS_ProductID",
+                table: "BASKET_POSITIONS",
+                column: "ProductID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BASKET_POSITIONS_UserID",
+                table: "BASKET_POSITIONS",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ORDER_POSITIONS_OrderID",
+                table: "ORDER_POSITIONS",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ORDERS_UserID",
+                table: "ORDERS",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BASKET_POSITIONS");
+
+            migrationBuilder.DropTable(
+                name: "ORDER_POSITIONS");
+
+            migrationBuilder.DropTable(
+                name: "PRODUCTS");
+
+            migrationBuilder.DropTable(
+                name: "ORDERS");
+
+            migrationBuilder.DropTable(
+                name: "USERS");
+        }
+    }
+}
